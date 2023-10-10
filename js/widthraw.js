a92d145a2bae353c65c9bba04a35ea283f55a87f
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-10-10 13:10:45 +0600

    Complete

--- js/widthraw.js
+++ js/widthraw.js
@@ -1,18 +1,26 @@
-document.getElementById("widthraw_btn").addEventListener("click", function () {
-  const widthraw = document.getElementById("widthraw_feild");
+function getByElemantId(input_feild) {
+  const widthraw = document.getElementById(input_feild);
   const widthrawPreviosAmountString = widthraw.value;
   const widthrawPreviosAmount = parseFloat(widthrawPreviosAmountString);
+  widthraw.value = "";
+  return widthrawPreviosAmount;
+}
 
-  const widthrawBalance = document.getElementById("widthraw_balance");
-  const widthrawBalanceString = widthrawBalance.innerText;
-  const widthrawBalanceTotal = parseFloat(widthrawBalanceString);
+function getElement(element) {
+  const total = document.getElementById(element);
+  const totalString = total.innerText;
+  const totalAmount = parseFloat(totalString);
+  return totalAmount;
+}
+
+document.getElementById("widthraw_btn").addEventListener("click", function () {
+  const widthrawPreviosAmount = getByElemantId("deposit_feild");
+  const widthrawBalanceTotal = getElement("total_balance");
 
   const newWidthrawBalance = widthrawBalanceTotal + widthrawPreviosAmount;
   widthrawBalance.innerText = newWidthrawBalance;
 
-  const balance = document.getElementById("total_balance");
-  const balancePreviosTotalString = balance.innerText;
-  const balancePreviosTotal = parseFloat(balancePreviosTotalString);
+  const balancePreviosTotal = getElement("total_balance");
 
   const newBalanceTotal = balancePreviosTotal - widthrawPreviosAmount;
   balance.innerText = newBalanceTotal;
